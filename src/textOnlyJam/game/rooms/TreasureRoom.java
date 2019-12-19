08c8b684cb8b95af97ab53348bed8351e0135190
Andreas Lau <andidebob@gmail.com>
2019-12-19 15:45:02 +0100

    editing pom

---
+++ src/textOnlyJam/game/rooms/TreasureRoom.java
@@ -0,0 +1,27 @@
+package textOnlyJam.game.rooms;
+
+public class TreasureRoom extends Room {
+
+	public TreasureRoom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String handleSpecific(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isSolved() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
